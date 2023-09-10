ea0505380b5c460f74a39b686974ddc969f95df9
Maciej Zieniuk <zieniuk.maciej@gmail.com>
2023-09-10 13:09:13 +0200

    passwordless api exception to include the details as part of exception message (for readability)

--- src/main/java/com/bitwarden/passwordless/error/PasswordlessApiException.java
+++ src/main/java/com/bitwarden/passwordless/error/PasswordlessApiException.java
@@ -2,11 +2,14 @@ package com.bitwarden.passwordless.error;
 
 import lombok.Getter;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class PasswordlessApiException extends Exception {
     @NonNull
     private final PasswordlessProblemDetails details;
+
+    public PasswordlessApiException(@NonNull PasswordlessProblemDetails details) {
+        super(details.toString());
+        this.details = details;
+    }
 }
