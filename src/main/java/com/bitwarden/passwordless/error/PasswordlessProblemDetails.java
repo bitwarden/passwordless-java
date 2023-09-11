364ec0dd55901695dd7df6bf675bc6f449a7eb29
Maciej Zieniuk <zieniuk.maciej@gmail.com>
2023-09-11 13:46:02 +0200

    problem details with "errors" in case of API payload validation

--- src/main/java/com/bitwarden/passwordless/error/PasswordlessProblemDetails.java
+++ src/main/java/com/bitwarden/passwordless/error/PasswordlessProblemDetails.java
@@ -2,6 +2,9 @@ package com.bitwarden.passwordless.error;
 
 import lombok.*;
 
+import java.util.List;
+import java.util.Map;
+
 @Data
 @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
 @AllArgsConstructor
@@ -13,4 +16,5 @@ public class PasswordlessProblemDetails {
     String detail;
     String instance;
     String errorCode;
+    Map<String, List<String>> errors;
 }
